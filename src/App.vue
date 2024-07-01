7cb69cee85a6c17833c07b2750ee8f4447bb5575
unknown <mutungaeric839@gmail.com>
2024-07-01 19:35:43 +0300

    working

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Medical Chatbot</h1>
+    <Chatbot />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Chatbot from './components/Chatbot_component.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Chatbot
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Add your global styles here */
 </style>
